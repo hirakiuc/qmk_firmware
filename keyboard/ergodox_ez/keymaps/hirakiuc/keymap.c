6dc928cc952a4ff108d4c0b21f4975019b484b5d
hirakiuc <hirakiuc@gmail.com>
2016-08-25 00:29:41 +0900

    use RGUI to right thumb part

--- keyboard/ergodox_ez/keymaps/hirakiuc/keymap.c
+++ keyboard/ergodox_ez/keymaps/hirakiuc/keymap.c
@@ -41,13 +41,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
         // right hand
-        KC_RGUI,        KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
+        KC_RGHT,        KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
         TG(SYMB),       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                         KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
         MEH_T(KC_NO),   KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                 KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
         KC_LALT,        CTL_T(KC_ESC),
-        KC_PGUP,
+        KC_RGUI,
         KC_ESC,         KC_TAB, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
