60816a1bb8e89454d16c21edac8b0a3d6b42c75b
hirakiuc <hirakiuc@gmail.com>
2016-09-10 13:29:49 +0900

    use MO(SYMB), not KC_FN1

--- keyboards/ergodox/ez/keymaps/hirakiuc/keymap.c
+++ keyboards/ergodox/ez/keymaps/hirakiuc/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_RPRN,        KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                         KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
         KC_RBRC,        KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
+                                KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          MO(SYMB),
         KC_LALT,        CTL_T(KC_ESC),
         KC_RGUI,
         KC_ESC,         KC_TAB, KC_ENT
