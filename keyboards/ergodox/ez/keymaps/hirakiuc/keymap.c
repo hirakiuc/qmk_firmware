5bd07d9416b4c29d03d7ec99d15df8a23844301c
hirakiuc <hirakiuc@gmail.com>
2016-11-26 10:28:23 +0900

    make the left-side bottom key as the left-ctrl

--- keyboards/ergodox/ez/keymaps/hirakiuc/keymap.c
+++ keyboards/ergodox/ez/keymaps/hirakiuc/keymap.c
@@ -20,15 +20,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  [   |           |   ]  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/Ctrl| '" |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | (SYMB) |
+ *   |LCtrl | Grv  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | (SYMB) |
  *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | R_Cmd|        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | Esc  |        |      |
- *                                 `--------------------'       `----------------------'
+ *                                      ,-------------.       ,-------------.
+ *                                      | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                               ,------|------|------|       |------+--------+------.
+ *                               |      |      | Home |       | R_Cmd|        |      |
+ *                               | Space|Backsp|------|       |------|  Tab   |Enter |
+ *                               |      |ace   | End  |       | Esc  |        |      |
+ *                               `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LPRN,
         MO(MDIA),       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
-        CTL_T(KC_GRV),  KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
+        KC_LCTRL,       KC_GRV,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
