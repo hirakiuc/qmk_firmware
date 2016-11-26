bab6720dcb2d14e019000c82115a4ae461bd2ce7
hirakiuc <hirakiuc@gmail.com>
2016-11-26 11:36:41 +0900

    add amethyst leader keymap

--- keyboards/ergodox/ez/keymaps/hirakiuc/keymap.c
+++ keyboards/ergodox/ez/keymaps/hirakiuc/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |LCtrl | Grv  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | (SYMB) |
  *   `----------------------------------'                                       `----------------------------------'
  *                                      ,-------------.       ,-------------.
- *                                      | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                      |AltShf| LGui |       | Alt  |Ctrl/Esc|
  *                               ,------|------|------|       |------+--------+------.
  *                               |      |      | Home |       | R_Cmd|        |      |
  *                               | Space|Backsp|------|       |------|  Tab   |Enter |
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(MDIA),       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
         KC_LCTRL,       KC_GRV,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
-                                              ALT_T(KC_APP),  KC_LGUI,
+                                              LALT(KC_LSFT),  KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
         // right hand
