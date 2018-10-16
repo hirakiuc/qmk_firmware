042e8a292482236cb68bfb3169b80f5d2b8d9b01
hirakiuc <hirakiuc@gmail.com>
2018-10-16 22:35:21 +0900

    update LOWER/RAISE layout to enable shift key

--- keyboards/fortitude60/keymaps/hirakiuc/keymap.c
+++ keyboards/fortitude60/keymaps/hirakiuc/keymap.c
@@ -59,9 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
  * |   ~  |      |      |   [  |   (  |   {  |             |   }  |   )  |   ]  |      |      |      |
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |             | LEFT | DOWN |   UP | RIGHT|   +  |  |   |
+ * | Shift|      |      |      |      |      |             | LEFT | DOWN |   UP | RIGHT|   +  |  |   |
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * |  F7  |  F8  |  F9  | F10  |  F11 |  F12 | ALSF | RGUI |      |      |      |      |      |      |
+ * | Ctrl |      |      |      |      |      | ALSF | RGUI |      |      |      |      |      |      |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
  *               | LGUI | LALT |      |  SPC | BSPC |  ENT |  ESC | RAISE| RALT | RGUI |
  *               `---------------------------------------------------------------------'
@@ -69,8 +69,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
   KC_RST,  _______, _______, KC_LBRC, KC_LPRN, KC_LCBR,                   KC_RCBR, KC_RPRN, KC_RBRC, _______, _______, _______, \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PLUS, KC_PIPE, \
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_ALSF, KC_RGUI, _______,  _______, _______,_______, _______, _______, \
+  KC_LSFT, _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PLUS, KC_PIPE, \
+  KC_LCTL, _______, _______, _______, _______, _______, KC_ALSF, KC_RGUI, _______,  _______, _______,_______, _______, _______, \
                     KC_LGUI, KC_LALT, _______, KC_SPC,  KC_BSPC, KC_ENT,  KC_ESC, RAISE, KC_RALT, KC_RGUI\
 ),
 
@@ -80,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------.             ,------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------.             ,------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   -  |   =  |   [  |   ]  |  |   |
+ * | Shift|  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   -  |   =  |   [  |   ]  |  |   |
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |      |  F12 |ISO # |ISO / |      |      |      |
+ * | Ctrl |  F7  |  F8  |  F9  |  F10 |  F11 |      |      |  F12 |ISO # |ISO / |      |      |      |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
  *               |      |      |      |      |      |      |      |      |      |      |
  *               `---------------------------------------------------------------------'
@@ -90,8 +90,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
   KC_GRV,  KC_1,    KC_UP,   KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_F4,   KC_F5,                      KC_F6,KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC,    KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_ALSF, KC_RGUI,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
+  KC_LSFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_F4,   KC_F5,                      KC_F6,KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC,    KC_BSLS, \
+  KC_LCTL, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_ALSF, KC_RGUI,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
                     KC_LGUI, KC_LALT, _______, KC_SPC,  KC_BSPC, KC_ENT,  KC_ESC, RAISE, KC_RALT, KC_RGUI\
 ),
 
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------.             ,------+------+------+------+------+------|
  * |      |      |      |Aud on|Audoff|AGnorm|             |AGswap|      |BL TOG|BL STP|      |      |
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * |Qwerty|Colemk|Dvorak|      |      |      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
  *               |      |      |      |      |      |      |      |      |      |      |
  *               `---------------------------------------------------------------------'
